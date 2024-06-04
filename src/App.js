e8cd70381e3b8b7e94c810b150decef2c9e7093b
Laura Yang <laurayang@lauras-mbp.student.wireless.sydney.edu.au>
2024-06-04 10:14:00 +1000

    added buttons 1-9

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@mui/material";
 
 function App() {
+  const numsOne = [1, 2, 3]
+  const numsTwo = [4, 5, 6]
+  const numsThree = [7, 8, 9]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+    <Box
+      height={500}
+      width={400}
+      border='2px, solid, black'
+      display='flex'
+      flexDirection='row'
+      justifyContent='center'
+    >
+      <div>
+        <Box
+          height={100}
+          width={350}
+          border='2px, solid, black'
+          marginTop='10px'
+          marginBottom='20px'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        </Box>
+        <div style={{ display:'flex', flexDirection:'col', justifyContent:'space-around' }}>
+          {numsOne.map((num) => 
+            <Box
+              height={65}
+              width={65}
+              border='2px, solid, black'
+              marginTop='10px'
+              display='flex'
+              justifyContent='center'
+              alignItems='center'
+              fontSize='25px'
+            >
+              {num}
+            </Box>
+          )}
+        </div> 
+        <div style={{ display:'flex', flexDirection:'col', justifyContent:'space-around' }}>
+          {numsTwo.map((num) => 
+            <Box
+              height={65}
+              width={65}
+              border='2px, solid, black'
+              marginTop='10px'
+              display='flex'
+              justifyContent='center'
+              alignItems='center'
+              fontSize='25px'
+            >
+              {num}
+            </Box>
+          )}
+        </div>  
+        <div style={{ display:'flex', flexDirection:'col', justifyContent:'space-around' }}>
+          {numsThree.map((num) => 
+            <Box
+              height={65}
+              width={65}
+              border='2px, solid, black'
+              marginTop='10px'
+              display='flex'
+              justifyContent='center'
+              alignItems='center'
+              fontSize='25px'
+            >
+              {num}
+            </Box>
+          )}
+        </div> 
+      </div>
+    </Box>
+    </>
   );
 }
 
